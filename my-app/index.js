19dcfb7a0b59ecc0c618f7db4c1f700624154c75
ORhan1993 <orhanbzgyk17@gmail.com>
2024-08-04 01:15:43 +0300

    Reaksiyon ZAmanı :D :D

--- my-app/index.js
+++ my-app/index.js
@@ -2,28 +2,80 @@
 
 // Filename - index.js
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+// import ReactDOM from 'react-dom/client';
+//import "./index.css";
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-// Component that will return an
-// unordered list
-function Navmenu(props)
-{
-    const list = props.menuitems;
+// Filename - index.js
 
-    const updatedList = list.map((listItems)=>{
-        return <li key={listItems.toString()}>{listItems}</li>;
-    });
+import ReactDOM from "react-dom";
 
-    return(
-        <ul>{updatedList}</ul>
-    );
+class App extends React.Component {
+    state = { username: "", email: "" };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            username: "gfg123",
+            email: "abc@gfg.org",
+        });
+    };
+
+    render() {  
+        return (
+            <div
+                style={{
+                    margin: "auto",
+                    marginTop: "20px",
+                    textAlign: "center",
+                }}
+            >
+                <form onSubmit={this.onFormSubmit}>
+                    <label> Enter username: </label>
+                    <input
+                        type="text"
+                        value={this.state.username}
+                        onChange={(e) =>
+                            this.setState((prev) => ({
+                                ...prev,
+                                username: e.target.value,
+                            }))
+                        }
+                    />
+                    <br />
+                    <br />
+                    <label>Enter Email Id:</label>
+                    <input
+                        type="email"
+                        value={this.state.email}
+                        onChange={(e) =>
+                            this.setState((prev) => ({
+                                ...prev,
+                                email: e.target.value,
+                            }))
+                        }
+                    ></input>
+                    <br />
+                    <br />
+                    <input type="submit" value={"Submit"} />
+                </form>
+                <br />
+                <div>
+                    Entered Value: {this.state.username}
+                </div>
+            </div>
+        );
+    }
 }
-
-const menuItems = [1,2,3,4,5];
-
-ReactDOM.render(
-    <Navmenu menuitems = {menuItems} />, 
-    document.getElementById('root')
+const root = ReactDOM.createRoot(
+    document.getElementById("root")
 );
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
 
