50650e533f836f12c136028631bc1976ad4c8fb1
ORhan1993 <orhanbzgyk17@gmail.com>
2024-08-04 00:52:49 +0300

    Fetih Başlasın React :D

--- my-app/index.js
+++ my-app/index.js
@@ -1,7 +1,29 @@
 // index.js
 
+// Filename - index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // Import your main React component
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Component that will return an
+// unordered list
+function Navmenu(props)
+{
+    const list = props.menuitems;
+
+    const updatedList = list.map((listItems)=>{
+        return <li key={listItems.toString()}>{listItems}</li>;
+    });
+
+    return(
+        <ul>{updatedList}</ul>
+    );
+}
+
+const menuItems = [1,2,3,4,5];
+
+ReactDOM.render(
+    <Navmenu menuitems = {menuItems} />, 
+    document.getElementById('root')
+);
+
