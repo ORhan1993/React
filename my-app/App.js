0edf51464af8d21465965383e466110d3846b5da
ORhan1993 <orhanbzgyk17@gmail.com>
2024-08-03 16:47:21 +0300

    React Bekle Beni Geliyorum :D :D

---
+++ my-app/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class App extends Component{
+   render(){
+      return(
+         <div>
+            <h1>Mühendisliğe dönüş hemen başlasın hemen!!!</h1>
+         </div>
+      );
+   }
+}
+export default App;
